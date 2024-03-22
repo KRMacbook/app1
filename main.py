9809bf0c9d4cb6078011ea4e78da9943675d821f
Mesimumm <64142436+KRMacbook@users.noreply.github.com>
2024-03-22 18:59:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate
 #from langchain.llms import OpenAI #so vananenud rida ning asendatud allolevaga
 from langchain_community.llms import OpenAI
 import os
